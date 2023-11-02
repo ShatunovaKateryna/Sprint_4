80ef4f05b6d09f4d7d0f144e88e1f09b05500968
ShatunovaKateryna <kateshatunova@gmail.com>
2023-11-02 02:07:34 +0200

    version2

--- src/StarFigure/StarFigure.java
+++ src/StarFigure/StarFigure.java
@@ -2,49 +2,25 @@ package StarFigure;
 
 public class StarFigure {
     //Shatunova Kateryna CS-12
-    public static final int HEIGHT=5;
-    public static final int WIDTH=2;
     public static void main(String[] args) {
-        for (int j=1;j<=WIDTH*8;j++){
+        for (int i = 1; i <= 16; i++) {
             System.out.print("/");
+        }
+        for (int j = 1; j <= 16; j++) {
+            System.out.print("\\");
+        }
+        System.out.println();
+        for (int k = 0; k < 4; k++) {
+            for (int j = 0; j < 12 - k * 4; j++) {
+                System.out.print("/");
             }
-        for (int j=1;j<=WIDTH*8;j++){
-            System.out.print("\\");
-        }
-        System.out.println();
-        for (int j=1;j<=WIDTH*6;j++){
-            System.out.print("/");
-        }
-        for (int j=1;j<=WIDTH*4;j++){
-            System.out.print("*");
-        }
-        for (int j=1;j<=WIDTH*6;j++){
-            System.out.print("\\");
-        }
-        System.out.println();
-        for (int j=1;j<=WIDTH*4;j++){
-            System.out.print("/");
-        }
-        for (int j=1;j<=WIDTH*8;j++){
-            System.out.print("*");
-        }
-        for (int j=1;j<=WIDTH*4;j++){
-            System.out.print("\\");
-        }
-        System.out.println();
-        for (int j=1;j<=WIDTH*2;j++){
-            System.out.print("/");
-        }
-        for (int j=1;j<=WIDTH*12;j++){
-            System.out.print("*");
-        }
-        for (int j=1;j<=WIDTH*2;j++){
-            System.out.print("\\");
-        }
-        System.out.println();
-        for (int j=1;j<=WIDTH*16;j++){
-            System.out.print("*");
+            for (int i = 0; i < 8 + k * 8; i++) {
+                System.out.print("*");
+            }
+            for (int i = 0; i < 12 - k * 4; i++) {
+                System.out.print("\\");
+            }
+            System.out.println();
         }
     }
-    }
-
+}
