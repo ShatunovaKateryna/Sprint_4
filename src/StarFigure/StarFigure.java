69c6c0bf18b77186482782a3c4b854e1b8b055f2
ShatunovaKateryna <kateshatunova@gmail.com>
2023-11-08 19:38:54 +0200

    final version

--- src/StarFigure/StarFigure.java
+++ src/StarFigure/StarFigure.java
@@ -2,22 +2,16 @@ package StarFigure;
 
 public class StarFigure {
     //Shatunova Kateryna CS-12
+    public static final int HEIGHT = 5;
     public static void main(String[] args) {
-        for (int i = 1; i <= 16; i++) {
-            System.out.print("/");
-        }
-        for (int j = 1; j <= 16; j++) {
-            System.out.print("\\");
-        }
-        System.out.println();
-        for (int k = 0; k < 4; k++) {
-            for (int j = 0; j < 12 - k * 4; j++) {
+        for (int i = 1; i <= HEIGHT; i++) {
+            for (int j = 1; j <= 4 * (-i + HEIGHT); j++) {
                 System.out.print("/");
             }
-            for (int i = 0; i < 8 + k * 8; i++) {
+            for (int k = 1; k <= 8 * i - 8; k++) {
                 System.out.print("*");
             }
-            for (int i = 0; i < 12 - k * 4; i++) {
+            for (int l = 1; l <= 4 * (-i + HEIGHT); l++) {
                 System.out.print("\\");
             }
             System.out.println();
